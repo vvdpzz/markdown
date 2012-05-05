349fa5f4cf733a51f6e915e30919d9d2c4c9a5d5
vvdpzz <vvdpzz@gmail.com>
2012-05-05 12:02:19 +0800

    Initial Commit

---
+++ markdown/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  markdown
+//
+//  Created by  on 12-5-5.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES;
+}
+
+@end
