cfe98d9b9f4a4ee5999dc4310120a2b3678e5a85
xxd <xuexiaodong79@gmail.com>
2012-06-20 10:59:44 +0800

    added:SettingPopoverController

---
+++ markdown/SettingPopoverController.h
@@ -0,0 +1,13 @@
+//
+//  SettingPopoverController.h
+//  markdown
+//
+//  Created by xiaodong xue on 12-6-20.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SettingPopoverController : NSObject
+
+@end
---
+++ markdown/SettingPopoverController.m
@@ -0,0 +1,13 @@
+//
+//  SettingPopoverController.m
+//  markdown
+//
+//  Created by xiaodong xue on 12-6-20.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "SettingPopoverController.h"
+
+@implementation SettingPopoverController
+
+@end
